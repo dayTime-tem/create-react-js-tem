a45ce2fb5a8c6cd47a3cfb9f838f17828d1de1e1
邓俊 <771238216@qq.com>
2021-06-10 17:37:10 +0800

    fix: 修改错误的组件命名

--- src/page/mainPage/mainPage.js
+++ src/page/mainPage/mainPage.js
@@ -1,10 +1,10 @@
 import React from "react";
 import style from './index.module.less'
-const mainPage = (props) => {
+const MinPage = (props) => {
     return (
         <div className={style.content}>
             这是主页面
         </div>
     )
 }
-export default mainPage
+export default MinPage
